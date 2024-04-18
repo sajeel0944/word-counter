f4644948a3f16b57ce2b50f5cccfc44bfb12cb8f
sajeek0944 <sajeel6y4z@gmail.com>
2024-04-18 17:29:09 +0500

    word counter

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue("\t============================"));
+console.log(chalk.yellowBright.bgMagenta("\tWELLCOME TO SUK WORD COUNTER"));
+console.log(chalk.blue("\t============================\n"));
+const ans = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: chalk.blueBright("inter your sentence"),
+});
+const modify = ans.sentence.trim().split(" ");
+let print = chalk.blueBright(`\nyour sentence word is = ${chalk.green(modify.length)}`);
+console.log(print);
